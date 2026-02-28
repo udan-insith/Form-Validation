b34029ab4df23623e8b28a2bdf98052ce37d3a80
udan-insith <udaninsith23613@gmail.com>
2026-02-28 17:45:01 +0530

    added functions to the buttons

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const username = document.getElementById("username")
 const password = document.getElementById("password")
 const errorMsg = document.querySelector(".error")
 const form = document.querySelector(".form")
+const loginBtn = document.querySelector(".login-btn")
+const clearBtn = document.querySelector(".clear-btn")
+
+loginBtn.addEventListener('click', function(e) {
+    let messages = []
+    if (username.value === '' || username.value == null) {
+        messages.push("Your Username is not available at the moment")
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMsg.innerText = messages.join(", ")
+    }
+})
+
+clearBtn.addEventListener('click', function(e) {
+    errorMsg.innerText = ""
+})
