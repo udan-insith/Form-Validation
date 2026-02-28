a4173654bb77d6deac13161a933a604d20465e6a
udan-insith <udaninsith23613@gmail.com>
2026-02-28 17:12:24 +0530

    added app js for gather elemnts

---
+++ app.js
@@ -0,0 +1,4 @@
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const errorMsg = document.querySelector(".error")
+const form = document.querySelector(".form")
